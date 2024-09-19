b1c0d525d4e7558113ddf499ef59ddc50e8fbd97
Anand Shekhar <66034323+anandshekhr@users.noreply.github.com>
2024-09-20 00:01:43 +0530

    ADD google one tap

--- src/bookstore/settings.py
+++ src/bookstore/settings.py
@@ -99,10 +99,24 @@ WSGI_APPLICATION = 'bookstore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',  # Use MySQL backend
+        'NAME': 'ashekhar123$vams-books-store',         # Your database name
+        'USER': 'ashekhar123',          # Your database username
+        'PASSWORD': 'OneManArmy1$',  # Your database password
+        'HOST': 'ashekhar123.mysql.pythonanywhere-services.com',                   # Set to your database server address (e.g., 'localhost' or '127.0.0.1')
+        # 'PORT': '3306',                        # Default MySQL port is 3306
+        # 'OPTIONS': {
+        #     'init_command':"SET SESSION max_allowed_packet=104857600",
+        #     },
     }
 }
 
@@ -233,3 +247,7 @@ HASHIDS_SALT = (
 
 DATA_UPLOAD_MAX_MEMORY_SIZE = 104857600
 FILE_UPLOAD_MAX_MEMORY_SIZE = 104857600
+
+
+SECURE_REFERRER_POLICY= "strict-origin-when-cross-origin"
+SOCIALACCOUNT_LOGIN_ON_GET=True
