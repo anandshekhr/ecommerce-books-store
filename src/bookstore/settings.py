676281010ed69b7ece2a1380fa744f347b276f18
Anand Shekhar <66034323+anandshekhr@users.noreply.github.com>
2024-09-27 21:53:58 +0530

    UPDATE pagination

--- src/bookstore/settings.py
+++ src/bookstore/settings.py
@@ -193,7 +193,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20
 }
 
