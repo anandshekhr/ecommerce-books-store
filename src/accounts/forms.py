31efd477be01855d02db43349815e05ed273c1d5
Anand Shekhar <66034323+anandshekhr@users.noreply.github.com>
2024-10-05 01:08:03 +0530

    ADD profile

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -3,13 +3,35 @@ from django.contrib.auth.models import User
 from .models import UserProfile
 
 class UserProfileForm(forms.ModelForm):
+    billing_address = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'class': 'form-control',
+            'rows': 5,  
+            'cols': 50, 
+        })
+    )
     class Meta:
         model = UserProfile
         fields = ['occupation', 'company_name', 'phone_number', 'bank_account_no', 'bank_name', 'ifsc_code', 'billing_address']
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        editable = kwargs.pop('editable', True)  # Add 'editable' flag
+        super(UserProfileForm, self).__init__(*args, **kwargs)
 
+        # If not editable, set fields to readonly/disabled
+        if not editable:
+            for field in self.fields.values():
+                field.widget.attrs['readonly'] = True
+                field.widget.attrs['disabled'] = True
+        
         for field_name, field in self.fields.items():
             if not isinstance(field.widget, forms.CheckboxInput):
                 field.widget.attrs['class'] = 'form-control'
+    
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+
+    #     for field_name, field in self.fields.items():
+    #         if not isinstance(field.widget, forms.CheckboxInput):
+    #             field.widget.attrs['class'] = 'form-control'
+    
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
 from .forms import UserProfileForm
 from django.contrib.auth.decorators import login_required
+from .models import UserProfile
 
 # Create your views here.
 
 @login_required
 def adminUserProfile(request):
+    profile, created = UserProfile.objects.get_or_create(user=request.user)
+    is_edit_mode = request.GET.get('edit', False)  # Check if the user wants to edit
+
     if request.method == 'POST':
-        form = UserProfileForm(request.POST, request.FILES)
+        form = UserProfileForm(request.POST, request.FILES, instance=profile, editable=True)
         if form.is_valid():
-            product = form.save(commit=False)
-            product.user = request.user
-            product.save()
+            form.save()
             return redirect('educator-admin-products')
     else:
-        form = UserProfileForm()
+        form = UserProfileForm(instance=profile, editable=is_edit_mode)
 
-    return render(request, 'educator/profile.html', {'form': form,'selected':'profile'})
+    return render(request, 'educator/profile.html', {
+        'form': form,
+        'selected': 'profile',
+        'is_edit_mode': is_edit_mode  # Pass this to template
+    })
